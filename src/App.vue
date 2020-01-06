739cdcd6fa20e0061811d5eb06c9e6cf1df0548f
xxd <826795224@qq.com>
2020-01-06 20:26:43 +0800

    项目初始化-封装注册vant组件模块

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
     <!-- 根路由出口 -->
-    <router-view></router-view>
+    <router-view />
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
   </div>
 </template>
 
@@ -12,5 +22,4 @@ export default {
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './utils/register-vant.js'
+
+// 加载
+
+// 注册
+// Vue.use(Button)
+// Vue.use(Cell)
+// Vue.use(CellGroup)
 
 Vue.config.productionTip = false
 
