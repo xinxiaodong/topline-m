f2d0d84abbff1d076e1df3cfd5a5e6e780ba2a4d
xxd <826795224@qq.com>
2020-01-08 17:19:24 +0800

    登录-准备-页面结构

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 
-import { Button, Cell, CellGroup, Icon } from 'vant'
+import { Button, Cell, CellGroup, Icon, NavBar, Field } from 'vant'
 
 Vue.use(Button)
   .use(Cell)
   .use(CellGroup)
   .use(Icon)
+  .use(NavBar)
+  .use(Field)
