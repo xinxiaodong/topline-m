cfa68c5e31e00f52345f0b2b3b9c4c630cbcd654
xxd <826795224@qq.com>
2020-01-09 17:11:49 +0800

    登录-表单验证-验证指定数据字段

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -17,7 +17,7 @@
         v-slot="{ errors }" 获取错误消息
      -->
     <ValidationObserver ref="form">
-      <ValidationProvider name="手机号" rules="required|mobile">
+      <ValidationProvider name="手机号" rules="required|mobile" immediate>
         <van-field
           class="form-item"
           v-model="user.mobile"
@@ -30,7 +30,7 @@
         <!-- <span>{{ errors[0] }}</span> -->
       </ValidationProvider>
 
-      <ValidationProvider name="验证码" rules="required|code">
+      <ValidationProvider name="验证码" rules="required|code" immediate>
         <van-field
           class="form-item"
           v-model="user.code"
@@ -94,22 +94,20 @@ export default {
 
       // 如果验证失败，提示错误消息，停止表单提交
       if (!success) {
-        // 这里加定时器的原因是因为获取验证失败的结果有延迟问题，并不是我们的原因
-        setTimeout(() => {
-          const errors = this.$refs.form.errors
-          // forEach 无法停止
-          // find 方法会遍历数组，对每个元素执行方法中的条件判定
-          // const item = Object.values(errors).find(item => item[0])
+        const errors = this.$refs.form.errors
+        // forEach 无法停止
+        // find 方法会遍历数组，对每个元素执行方法中的条件判定
+        // const item = Object.values(errors).find(item => item[0])
 
-          // 找到第1个有错误的字段并弹出提示
-          for (let key in errors) {
-            const item = errors[key]
-            if (item[0]) {
-              this.$toast(item[0])
-              return
-            }
+        // 找到第1个有错误的字段并弹出提示
+        for (let key in errors) {
+          const item = errors[key]
+          if (item[0]) {
+            this.$toast(item[0])
+            return
           }
-        }, 100)
+        }
+
         return
       }
 
@@ -129,7 +127,7 @@ export default {
         this.$toast.success('登录成功')
       } catch (err) {
         console.log('登录失败', err)
-        this.$toast.fail('登录失败')
+        this.$toast.fail('登录失败，手机号或验证码不正确')
       }
 
       // 4. 根据接口返回结果执行后续处理
