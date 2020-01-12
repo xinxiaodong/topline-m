bb551fa3c3c6b94a686f1535e2348703923b7a0c
xxd <826795224@qq.com>
2020-01-12 15:47:47 +0800

    个人中心-展示当前登录用户信息

--- src/api/user.js
+++ src/api/user.js
@@ -20,3 +20,11 @@ export const getSmsCode = mobile => {
     url: `/app/v1_0/sms/codes/${mobile}`
   })
 }
+
+// 获取当前登录用户信息
+export const getUserInfo = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user'
+  })
+}
