c6c0f5a14351c6ed830116e87268e500851a32ef
xxd <826795224@qq.com>
2020-01-12 22:10:54 +0800

    频道编辑-数据持久化

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -2,6 +2,11 @@
  * 封装操作本地存储的工具方法模块
  */
 
+/**
+ * 获取指定数据
+ * @param {String} 数据名称
+ */
+
 export const getItem = name => {
   const data = window.localStorage.getItem(name)
   try {
@@ -12,12 +17,23 @@ export const getItem = name => {
   }
 }
 
+/**
+ * 存储数据
+ * @param {String} 数据名称
+ * @param {} 数据值
+ */
+
 export const setItem = (name, value) => {
   const data = typeof value === 'object'
     ? JSON.stringify(value)
     : value
   window.localStorage.setItem(name, data)
 }
+
+/**
+ * 删除数据
+ * @param {String} 数据名称
+ */
 
 export const removeItem = name => {
   window.localStorage.removeItem(name)
