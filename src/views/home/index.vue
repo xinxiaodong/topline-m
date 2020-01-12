60d5d7c7ced0bf5a9ab13d3cb91ea1cf58a9fa34
xxd <826795224@qq.com>
2020-01-12 22:03:22 +0800

    频道编辑-切换频道

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -33,7 +33,7 @@
       closeable
       close-icon-position="top-left"
     >
-     <channel-edit :user-channels="userChannels" />
+     <channel-edit :user-channels="userChannels" @switch="onChannelSwitch" />
     </van-popup>
     <!-- /编辑频道 -->
   </div>
@@ -72,6 +72,10 @@ export default {
         console.log(err)
         this.$toast('获取频道数据失败')
       }
+    },
+    onChannelSwitch (index) {
+      this.active = index // 切换激活频道
+      this.isChannelEditShow = false // 关闭弹层
     }
   }
 }
