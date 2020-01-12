d06c9265ac5d16b80430a095af1157a6c622f7a3
xxd <826795224@qq.com>
2020-01-12 21:24:37 +0800

    首页文章列表-下拉刷新

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 
-import { Button, Cell, CellGroup, Icon, NavBar, Field, Toast, CountDown, Tabbar, TabbarItem, Image, Grid, GridItem, Dialog, List, Tab, Tabs } from 'vant'
+import { Button, Cell, CellGroup, Icon, NavBar, Field, Toast, CountDown, Tabbar, TabbarItem, Image, Grid, GridItem, Dialog, List, Tab, Tabs, PullRefresh } from 'vant'
 
 Vue.use(Button)
   .use(Cell)
@@ -19,3 +19,4 @@ Vue.use(Button)
   .use(List)
   .use(Tab)
   .use(Tabs)
+  .use(PullRefresh)
