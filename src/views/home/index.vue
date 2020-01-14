a708c6f562bcf3ec341fa722a7aa227c12f56e0a
xxd <826795224@qq.com>
2020-01-14 21:15:34 +0800

    调整首页导航栏样式

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="home-container">
     <!-- 导航栏 -->
-    <van-nav-bar title="首页" fixed />
+    <div class="nav-bar">
+      <div class="logo"></div>
+      <van-button
+        class="search-btn"
+        round
+        type="info"
+        size="small"
+        icon="search"
+        @click="$router.push('/search')"
+      >搜索</van-button>
+    </div>
     <!-- /导航栏 -->
 
     <!-- 频道列表 -->
@@ -11,20 +21,15 @@
     -->
     <van-tabs v-model="active">
       <!-- 通过插槽把面包按钮放进去 -->
-      <van-icon
-        class="wap-nav"
-        slot="nav-right"
-        name="wap-nav"
-        @click="isChannelEditShow = true"
-      />
+      <van-icon class="wap-nav" slot="nav-right" name="wap-nav" @click="isChannelEditShow = true" />
       <van-tab :title="channel.name" v-for="channel in userChannels" :key="channel.id">
         <!-- 文章列表 -->
-      <article-list :channel="channel" />
+        <article-list :channel="channel" />
         <!-- /文章列表 -->
       </van-tab>
     </van-tabs>
     <!-- /频道列表 -->
-        <!-- 编辑频道 -->
+    <!-- 编辑频道 -->
     <van-popup
       v-model="isChannelEditShow"
       position="bottom"
@@ -33,7 +38,7 @@
       closeable
       close-icon-position="top-left"
     >
-     <channel-edit :user-channels="userChannels"  :active="active" @switch="onChannelSwitch" />
+      <channel-edit :user-channels="userChannels" :active="active" @switch="onChannelSwitch" />
     </van-popup>
     <!-- /编辑频道 -->
   </div>
@@ -98,19 +103,42 @@ export default {
 .home-container {
   padding-top: 90px;
   padding-bottom: 50px;
-}
-.wap-nav {
-  position: fixed;
-  right: 0;
-  line-height: 44px;
-  background: #fff;
-  opacity: .8;
-}
-/deep/ .van-tabs__wrap {
-  position: fixed;
-  top: 46px;
-  left: 0;
-  right: 0;
-  z-index: 1;
+  .nav-bar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 15px;
+    height: 46px;
+    background-color: #3196fa;
+    z-index: 1;
+    .logo {
+      background: url("./logo-light.png") no-repeat;
+      background-size: cover;
+      width: 100px;
+      height: 30px;
+    }
+    .search-btn {
+      background-color: #5babfb;
+      width: 50%;
+    }
+  }
+  .wap-nav {
+    position: fixed;
+    right: 0;
+    line-height: 44px;
+    background: #fff;
+    opacity: 0.8;
+  }
+  /deep/ .van-tabs__wrap {
+    position: fixed;
+    top: 46px;
+    left: 0;
+    right: 0;
+    z-index: 1;
+  }
 }
 </style>
