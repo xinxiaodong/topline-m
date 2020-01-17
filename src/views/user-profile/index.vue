1456c32ed1c065a301a2193f2f7b8d8ba38c9534
xxd <826795224@qq.com>
2020-01-17 16:01:18 +0800

    编辑用户资料-展示用户资料

--- src/views/user-profile/index.vue
+++ src/views/user-profile/index.vue
@@ -1,45 +1,52 @@
 <template>
- <div class="user-profile">
-    <van-nav-bar
-      title="编辑资料"
-      left-arrow
-      @click-left="$router.back()"
-    />
+  <div class="user-profile">
+    <van-nav-bar title="编辑资料" left-arrow @click-left="$router.back()" />
 
     <van-cell-group>
       <van-cell is-link title="头像">
-        <van-image
-          class="avatar"
-          round
-          src="https://img.yzcdn.cn/vant/cat.jpeg"
-        />
+        <van-image class="avatar" round :src="user.photo" />
       </van-cell>
-      <van-cell is-link title="昵称" value="内容" />
+      <van-cell is-link title="昵称" :value="user.name" />
       <van-cell is-link title="介绍" value="内容" />
-      <van-cell is-link title="性别" value="内容" />
-      <van-cell is-link title="生日" value="内容" />
+      <van-cell is-link title="性别" :value="user.gender === 0 ? '男' : '女'" />
+      <van-cell is-link title="生日" :value="user.birthday" />
     </van-cell-group>
   </div>
 </template>
 
 <script>
+import { getUserProfile } from '@/api/user'
 export default {
   name: 'UserProfile',
   components: {},
   props: {},
   data () {
-    return {}
+    return {
+      user: {} // 用户资料
+    }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadUserProfile()
+  },
   mounted () {},
-  methods: {}
+  methods: {
+    async loadUserProfile () {
+      try {
+        const { data } = await getUserProfile()
+        this.user = data.data
+      } catch (err) {
+        console.log(err)
+        this.$toast.fail('获取数据失败')
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
- .user-profile {
+.user-profile {
   .avatar {
     width: 30px;
     height: 30px;
